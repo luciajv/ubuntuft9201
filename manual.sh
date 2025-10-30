8f0d80cb893066e0475c8a66d50b2dc1168e11dd
lu <91312264+luciajv@users.noreply.github.com>
2025-10-30 12:41:38 -0300

    Update manual.sh

--- manual.sh
+++ manual.sh
@@ -1,11 +1,6 @@
-# Clonar libfprint
 git clone https://gitlab.freedesktop.org/libfprint/libfprint.git
 cd libfprint
-
-# Aplicar parche
 git apply patches/add-ft9201-driver.patch
-
-# Compilar e instalar
 meson setup build
 ninja -C build
 sudo ninja -C build install
